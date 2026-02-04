ec299929a0076457b74be8d8b74e8c79d355e402
Shkoda <gamerbeimax@gmail.com>
2026-02-04 19:20:20 +0200

    Add files via upload

---
+++ hz/lesson6.2.py
@@ -0,0 +1,89 @@
+import warnings
+
+# def clcl(a, b):
+#     if b == 0:
+#         warnings.warn('не діли на нуль (я попередив)', UserWarning)
+#         return 'Я попередив . . .'
+#
+#     else:
+#         return(a/b)
+# print('a / b :')
+# print(clcl(2, 0))
+# print('a / b №2 :')
+# print(clcl(3, 6))
+
+
+
+warnings.simplefilter('always', SyntaxWarning)
+warnings.simplefilter('always', UserWarning)
+
+warnings.warn('DO NOT PRINT ZERO', UserWarning)
+print('Hello.')
+input(': ')
+print('What do you want to print? (number)')
+x = int(input(': '))
+print('Alright')
+if x == 0:
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+
+    print(0 / 0)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+    warnings.warn('NOO', UserWarning)
+    warnings.warn('HOW DARE YOU', UserWarning)
+    warnings.warn('YOU MONSTER', UserWarning)
+else:
+    warnings.warn('thanks bro', UserWarning)
+
+
+
+
+
+
+
+
